aef698209fb72561e1c1c96dbdcc039be4d9cda6
bry0306 <37124301+bry0306@users.noreply.github.com>
2024-12-24 02:14:31 -0500

    kj

--- app/layoutClient.tsx
+++ app/layoutClient.tsx
@@ -4,10 +4,14 @@
 
 import { Analytics } from "@vercel/analytics/react"
 import { Toaster } from "@/components/ui/toaster"
+import { useRef, useState } from 'react';
+import { Cursor } from '@/components/ui/cursor';
+import { AnimatePresence, motion } from 'motion/react';
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
+    
       {children}
       <div className="fixed inset-x-0 bottom-0 z-[9999] pointer-events-auto">
         
